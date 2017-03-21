bb83440d91c61ba9cf232ad2cfea6d282f196fdf
Dale Fugier <dale@mcneel.com>
2017-03-21 14:17:38 -0700

    Added SampleCsWpf

---
+++ rhinocommon/cs/SampleCsWpf/Commands/SampleCsWpfPanel.cs
@@ -0,0 +1,60 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Input.Custom;
+using Rhino.UI;
+
+namespace SampleCsWpf.Commands
+{
+  [System.Runtime.InteropServices.Guid("664de49d-11f7-4de8-9c7f-26f768685e21")]
+  public class SampleCsWpfPanel : Command
+  {
+    public override string EnglishName
+    {
+      get { return "SampleCsWpfPanel"; }
+    }
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      var panel_id = SampleCsWpfPanelHost.PanelId;
+      var panel_visible = Panels.IsPanelVisible(panel_id);
+
+      var prompt = (panel_visible)
+        ? "Sample panel is visible. New value"
+        : "Sample Manager panel is hidden. New value";
+
+      var go = new GetOption();
+      go.SetCommandPrompt(prompt);
+      var hide_index = go.AddOption("Hide");
+      var show_index = go.AddOption("Show");
+      var toggle_index = go.AddOption("Toggle");
+      go.Get();
+
+      if (go.CommandResult() != Result.Success)
+        return go.CommandResult();
+
+      var option = go.Option();
+      if (null == option)
+        return Result.Failure;
+
+      var index = option.Index;
+      if (index == hide_index)
+      {
+        if (panel_visible)
+          Panels.ClosePanel(panel_id);
+      }
+      else if (index == show_index)
+      {
+        if (!panel_visible)
+          Panels.OpenPanel(panel_id);
+      }
+      else if (index == toggle_index)
+      {
+        if (panel_visible)
+          Panels.ClosePanel(panel_id);
+        else
+          Panels.OpenPanel(panel_id);
+      }
+      return Result.Success;
+    }
+  }
+}
---
+++ rhinocommon/cs/SampleCsWpf/SampleCsWpfPlugIn.cs
@@ -0,0 +1,56 @@
+﻿using Rhino.PlugIns;
+using Rhino.UI;
+using SampleCsWpf.Forms;
+
+namespace SampleCsWpf
+{
+  ///<summary>
+  /// Every RhinoCommon plug-in must have one and only one Rhino.PlugIns.PlugIn
+  /// inherited class. DO NOT create instances of this class yourself. It is the
+  /// responsibility of Rhino to create an instance of this class.
+  ///</summary>
+  public class SampleCsWpfPlugIn : Rhino.PlugIns.PlugIn
+  {
+    public SampleCsWpfPlugIn()
+    {
+      Instance = this;
+    }
+
+    ///<summary>Gets the only instance of the SampleCsWpfPlugIn plug-in.</summary>
+    public static SampleCsWpfPlugIn Instance
+    {
+      get;
+      private set;
+    }
+
+    protected override LoadReturnCode OnLoad(ref string errorMessage)
+    {
+      System.Type panel_type = typeof(SampleCsWpfPanelHost);
+      Panels.RegisterPanel(this, panel_type, "SampleWpf", Properties.Resources.SampleCsWpfPanel);
+      return LoadReturnCode.Success;
+    }
+  }
+
+  /// <summary>
+  /// Rhino framework requires a System.Windows.Forms.IWin32Window derived object for a panel.
+  /// </summary>
+  [System.Runtime.InteropServices.Guid("AB80CF1B-B499-42EC-B25D-EF1B41220C7D")]
+  public class SampleCsWpfPanelHost : RhinoWindows.Controls.WpfElementHost
+  {
+    public SampleCsWpfPanelHost()
+      : base(new SampleCsWpfPanelUserControl(), null) // No view model (for this example)
+    {
+    }
+
+    /// <summary>
+    /// Returns the ID of this panel.
+    /// </summary>
+    public static System.Guid PanelId
+    {
+      get
+      {
+        return typeof(SampleCsWpfPanelHost).GUID;
+      }
+    }
+  }
+}
