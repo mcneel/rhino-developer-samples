d2609d802a8f07c049008f03e9935e4ee0f6bc0c
Dale Fugier <dale@mcneel.com>
2017-03-21 15:12:48 -0700

    Added addition WinForm examples.

---
+++ rhinocommon/cs/SampleCsWinForms/Forms/SampleCsFibonacciForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace SampleCsWinForms.Forms
+{
+  public partial class SampleCsFibonacciForm : Form
+  {
+    private int m_number_to_compute;
+    private int m_highest_percentage_reached;
+
+    public SampleCsFibonacciForm()
+    {
+      InitializeComponent();
+      InitializeBackgroundWorker();
+    }
+
+    private void InitializeBackgroundWorker()
+    {
+      m_background_worker.DoWork += DoWork;
+      m_background_worker.RunWorkerCompleted += RunWorkerCompleted;
+      m_background_worker.ProgressChanged += ProgressChanged;
+    }
+
+    /// <summary>
+    /// OnStartButtonClick
+    /// </summary>
+    private void OnStartButtonClick(System.Object sender, System.EventArgs e)
+    {
+      // Reset the text in the result label.
+      resultLabel.Text = String.Empty;
+
+      // Disable the UpDown control until the asynchronous operation is done.
+      m_spinner.Enabled = false;
+
+      // Disable the Start button until the asynchronous operation is done.
+      m_start_button.Enabled = false;
+
+      // Enable the Cancel button while the asynchronous operation runs.
+      m_cancel_button.Enabled = true;
+
+      // Get the value from the UpDown control.
+      m_number_to_compute = (int)m_spinner.Value;
+
+      // Reset the variable for percentage tracking.
+      m_highest_percentage_reached = 0;
+
+      // Start the asynchronous operation.
+      m_background_worker.RunWorkerAsync(m_number_to_compute);
+    }
+
+    /// <summary>
+    /// OnCancelButtonClick
+    /// </summary>
+    private void OnCancelButtonClick(System.Object sender, System.EventArgs e)
+    {
+      // Cancel the asynchronous operation.
+      this.m_background_worker.CancelAsync();
+
+      // Disable the Cancel button.
+      m_cancel_button.Enabled = false;
+    }
+
+    /// <summary>
+    /// This event handler is where the actual, potentially time-consuming work is done.
+    /// </summary>
+    private void DoWork(object sender, DoWorkEventArgs e)
+    {
+      // Get the BackgroundWorker that raised this event.
+      var worker = sender as BackgroundWorker;
+
+      // Assign the result of the computation to the Result property of the DoWorkEventArgs
+      // object. This is will be available to the RunWorkerCompleted eventhandler.
+      e.Result = ComputeFibonacci((int)e.Argument, worker, e);
+    }
+
+    /// <summary>
+    /// This event handler deals with the results of the background operation.
+    /// </summary>
+    private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+      // First, handle the case where an exception was thrown.
+      if (e.Error != null)
+      {
+        MessageBox.Show(e.Error.Message);
+      }
+      else if (e.Cancelled)
+      {
+        // Next, handle the case where the user canceled the operation.
+        // Note that due to a race condition in the DoWork event handler, the Cancelled
+        // flag may not have been set, even though CancelAsync was called.
+        resultLabel.Text = "Canceled";
+      }
+      else
+      {
+        // Finally, handle the case where the operation succeeded.
+        resultLabel.Text = e.Result.ToString();
+      }
+
+      // Enable the UpDown control.
+      m_spinner.Enabled = true;
+
+      // Enable the Start button.
+      m_start_button.Enabled = true;
+
+      // Disable the Cancel button.
+      m_cancel_button.Enabled = false;
+    }
+
+    /// <summary>
+    /// This event handler updates the progress bar.
+    /// </summary>
+    private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+      m_progress_bar.Value = e.ProgressPercentage;
+    }
+
+    /// <summary>
+    /// This is the method that does the actual work. For this example, 
+    /// it computes a Fibonacci number and reports progress as it does its work.
+    /// </summary>
+    private long ComputeFibonacci(int n, BackgroundWorker worker, DoWorkEventArgs e)
+    {
+      // The parameter n must be >= 0 and <= 91.
+      // Fib(n), with n > 91, overflows a long.
+      if (n < 0 || n > 91)
+      {
+        throw new ArgumentException("Value must be >= 0 and <= 91", "n");
+      }
+
+      long result = 0;
+
+      // Abort the operation if the user has canceled. Note that a call to CancelAsync may have set 
+      // CancellationPending to true just after the last invocation of this method exits, so this 
+      // code will not have the opportunity to set the DoWorkEventArgs.Cancel flag to true. This means
+      // that RunWorkerCompletedEventArgs.Cancelled will not be set to true in your RunWorkerCompleted
+      // event handler. This is a race condition.
+
+      if (worker.CancellationPending)
+      {
+        e.Cancel = true;
+      }
+      else
+      {
+        if (n < 2)
+          result = 1;
+        else
+          result = ComputeFibonacci(n - 1, worker, e) + ComputeFibonacci(n - 2, worker, e);
+
+        // Report progress as a percentage of the total task.
+        var percent_complete = (int)(n / (float)m_number_to_compute * 100);
+        if (percent_complete > m_highest_percentage_reached)
+        {
+          m_highest_percentage_reached = percent_complete;
+          worker.ReportProgress(percent_complete);
+        }
+      }
+
+      return result;
+    }
+  }
+}
---
+++ rhinocommon/cs/SampleCsWinForms/Forms/SampleCsFibonacciForm.designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SampleCsWinForms.Forms
+{
+  partial class SampleCsFibonacciForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.m_progress_bar = new System.Windows.Forms.ProgressBar();
+      this.resultLabel = new System.Windows.Forms.Label();
+      this.m_cancel_button = new System.Windows.Forms.Button();
+      this.m_start_button = new System.Windows.Forms.Button();
+      this.m_background_worker = new System.ComponentModel.BackgroundWorker();
+      this.m_spinner = new System.Windows.Forms.NumericUpDown();
+      ((System.ComponentModel.ISupportInitialize)(this.m_spinner)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // progressBar1
+      // 
+      this.m_progress_bar.Location = new System.Drawing.Point(12, 47);
+      this.m_progress_bar.Name = "m_progress_bar";
+      this.m_progress_bar.Size = new System.Drawing.Size(268, 8);
+      this.m_progress_bar.Step = 2;
+      this.m_progress_bar.TabIndex = 9;
+      // 
+      // resultLabel
+      // 
+      this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.resultLabel.Location = new System.Drawing.Point(98, 12);
+      this.resultLabel.Name = "resultLabel";
+      this.resultLabel.Size = new System.Drawing.Size(182, 23);
+      this.resultLabel.TabIndex = 8;
+      this.resultLabel.Text = "(no result)";
+      this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // cancelAsyncButton
+      // 
+      this.m_cancel_button.Enabled = false;
+      this.m_cancel_button.Location = new System.Drawing.Point(161, 61);
+      this.m_cancel_button.Name = "m_cancel_button";
+      this.m_cancel_button.Size = new System.Drawing.Size(119, 23);
+      this.m_cancel_button.TabIndex = 7;
+      this.m_cancel_button.Text = "Cancel";
+      this.m_cancel_button.Click += new System.EventHandler(this.OnCancelButtonClick);
+      // 
+      // startAsyncButton
+      // 
+      this.m_start_button.Location = new System.Drawing.Point(12, 61);
+      this.m_start_button.Name = "m_start_button";
+      this.m_start_button.Size = new System.Drawing.Size(120, 23);
+      this.m_start_button.TabIndex = 6;
+      this.m_start_button.Text = "Start";
+      this.m_start_button.Click += new System.EventHandler(this.OnStartButtonClick);
+      // 
+      // backgroundWorker1
+      // 
+      this.m_background_worker.WorkerReportsProgress = true;
+      this.m_background_worker.WorkerSupportsCancellation = true;
+      // 
+      // numericUpDown1
+      // 
+      this.m_spinner.Location = new System.Drawing.Point(12, 12);
+      this.m_spinner.Maximum = new decimal(new int[] {
+            91,
+            0,
+            0,
+            0});
+      this.m_spinner.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+      this.m_spinner.Name = "m_spinner";
+      this.m_spinner.Size = new System.Drawing.Size(80, 20);
+      this.m_spinner.TabIndex = 5;
+      this.m_spinner.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+      // 
+      // SampleCsFibonacciForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(292, 118);
+      this.Controls.Add(this.m_progress_bar);
+      this.Controls.Add(this.resultLabel);
+      this.Controls.Add(this.m_cancel_button);
+      this.Controls.Add(this.m_start_button);
+      this.Controls.Add(this.m_spinner);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "SampleCsFibonacciForm";
+      this.ShowIcon = false;
+      this.ShowInTaskbar = false;
+      this.Text = "Fibonacci Calculator";
+      ((System.ComponentModel.ISupportInitialize)(this.m_spinner)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ProgressBar m_progress_bar;
+    private System.Windows.Forms.Label resultLabel;
+    private System.Windows.Forms.Button m_cancel_button;
+    private System.Windows.Forms.Button m_start_button;
+    private System.ComponentModel.BackgroundWorker m_background_worker;
+    private System.Windows.Forms.NumericUpDown m_spinner;
+  }
+}
