7da4e22e63b4fd3091d45b7728f4746cb149816c
Dale Fugier <dale@mcneel.com>
2017-03-21 10:35:32 -0700

    Add SampleCsSkin project

---
+++ rhinocommon/cs/SampleCsSkin/SampleCsSplashScreenWorker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace SampleCsSkin
+{
+  public class SampleCsSplashScreenWorker
+  {
+    public event EventHandler<SplashScreenWorkerArgs> ProgressChanged;
+    public event EventHandler HardWorkDone;
+
+    public void DoHardWork()
+    {
+      // Do something time consuming...
+      for (var i = 1; i <= 100; i++)
+      {
+        for (var j = 1; j <= 100000; j++)
+        {
+          Thread.Sleep(0);
+        }
+        OnProgressChanged(i);
+      }
+
+      OnWorkerDone();
+    }
+
+    private void OnProgressChanged(int progress)
+    {
+      var handler = ProgressChanged;
+      if (handler != null)
+      {
+        handler(this, new SplashScreenWorkerArgs(progress));
+      }
+    }
+
+    private void OnWorkerDone()
+    {
+      var handler = HardWorkDone;
+      if (handler != null)
+      {
+        handler(this, EventArgs.Empty);
+      }
+    }
+  }
+
+  public class SplashScreenWorkerArgs : EventArgs
+  {
+    public SplashScreenWorkerArgs(int progress)
+    {
+      Progress = progress;
+    }
+
+    public int Progress
+    {
+      get;
+      private set;
+    }
+  }
+}
---
+++ rhinocommon/cs/SampleCsSkin/SplashScreen.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace SampleCsSkin
+{
+  partial class SplashScreen
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.progressBar = new System.Windows.Forms.ProgressBar();
+      this.SuspendLayout();
+      // 
+      // progressBar
+      // 
+      this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.progressBar.Location = new System.Drawing.Point(0, 318);
+      this.progressBar.Name = "progressBar";
+      this.progressBar.Size = new System.Drawing.Size(488, 23);
+      this.progressBar.TabIndex = 0;
+      // 
+      // SplashScreen
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackgroundImage = global::SampleCsSkin.Properties.Resources.SplashScreen;
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+      this.ClientSize = new System.Drawing.Size(488, 341);
+      this.ControlBox = false;
+      this.Controls.Add(this.progressBar);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "SplashScreen";
+      this.ShowIcon = false;
+      this.ShowInTaskbar = false;
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "SplashScreen";
+      this.TopMost = true;
+      this.Load += new System.EventHandler(this.SplashScreen_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ProgressBar progressBar;
+  }
+}
