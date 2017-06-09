ad9a9455ff173cb17c8db014f2d9c618a724ad5c
Dale Fugier <dale@mcneel.com>
2017-06-09 13:32:43 -0700

    Added SampleCsMobilePlane project

---
+++ rhinocommon/cs/SampleCsMobilePlane/SampleCsMobilePlaneConduit.cs
@@ -0,0 +1,34 @@
+﻿using Rhino.ApplicationSettings;
+using Rhino.Display;
+using Rhino.Geometry;
+
+namespace SampleCsMobilePlane
+{
+  /// <summary>
+  /// SampleCsMobilePlaneConduit
+  /// </summary>
+  public class SampleCsMobilePlaneConduit : DisplayConduit
+  {
+    public Plane Plane { get; private set; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public SampleCsMobilePlaneConduit(Plane plane)
+    {
+      Plane = plane;
+      Enabled = true;
+    }
+
+    /// <summary>
+    /// Draw direction arrows using the plane member.
+    /// </summary>
+    /// <param name="e"></param>
+    protected override void DrawOverlay(DrawEventArgs e)
+    {
+      e.Display.DrawDirectionArrow(Plane.Origin, Plane.XAxis, AppearanceSettings.GridXAxisLineColor);
+      e.Display.DrawDirectionArrow(Plane.Origin, Plane.YAxis, AppearanceSettings.GridYAxisLineColor);
+      e.Display.DrawDirectionArrow(Plane.Origin, Plane.ZAxis, AppearanceSettings.GridZAxisLineColor);
+    }
+  }
+}
