d3e89485f3db9dac46b9d0d06e60166dadbba232
Dale Fugier <dale@mcneel.com>
2017-12-04 11:07:05 -0800

    Updated event watcher sample

--- rhinocommon/cs/SampleCsEventWatcher/SampleCsEventHandlers.cs
+++ rhinocommon/cs/SampleCsEventWatcher/SampleCsEventHandlers.cs
@@ -475,7 +475,7 @@ namespace SampleCsEventWatcher
     /// </summary>
     public static void OnBeginCommand(object sender, CommandEventArgs e)
     {
-      DebugWriteMethod();
+      RhinoApp.WriteLine("> BeginCommand ({0})", e.CommandEnglishName);
     }
 
     /// <summary>
@@ -483,7 +483,7 @@ namespace SampleCsEventWatcher
     /// </summary>
     public static void OnEndCommand(object sender, CommandEventArgs e)
     {
-      DebugWriteMethod();
+      RhinoApp.WriteLine("> EndCommand ({0})", e.CommandEnglishName);
     }
 
     /// <summary>
