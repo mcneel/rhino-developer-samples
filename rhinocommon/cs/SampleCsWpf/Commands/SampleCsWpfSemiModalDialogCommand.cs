ac1206e134fae1c831ff415dbdcf19ffded2e7fa
Dale Fugier <dale@mcneel.com>
2018-03-01 13:36:21 -0800

    Added SampleCsWpfSemiModalDialogCommand.cs

---
+++ rhinocommon/cs/SampleCsWpf/Commands/SampleCsWpfSemiModalDialogCommand.cs
@@ -0,0 +1,19 @@
+﻿using Rhino;
+using Rhino.Commands;
+using RhinoWindows;
+
+namespace SampleCsWpf.Commands
+{
+  public class SampleCsWpfSemiModalDialogCommand : Command
+  {
+    public override string EnglishName => "SampleCsWpfSemiModalDialog";
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      var dialog = new Views.SampleCsWpfDialog();
+      dialog.ShowSemiModal(RhinoApp.MainWindowHandle());
+
+      return Result.Success;
+    }
+  }
+}
