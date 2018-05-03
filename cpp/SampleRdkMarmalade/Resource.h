917f1cd345ab40f40f369c1a37186d2fecbcdf92
John Croudy <john.croudy@mcneel.com>
2018-05-02 17:33:53 -0700

    Fixes project and resource file problems.

--- cpp/SampleRdkMarmalade/Resource.h
+++ cpp/SampleRdkMarmalade/Resource.h
@@ -7,29 +7,28 @@
 #define IDM_APP_FILE_PRINT              103
 #define IDM_APP_FILE_EXIT               104
 #define IDM_APP_HELP_DISPLAY_HELP       105
-#define IDI_ICON1                       3000
+#define IDR_RENDER                      3000
 #define IDI_MARMALADE                   3000
-#define IDD_OPTIONS_SECTION_1           3001
-#define IDC_CHECK1                      3001
-#define IDC_CHECK2                      3002
-#define IDD_OPTIONS_SECTION_2           3002
-#define IDC_CHECK_EXPERT_MODE           3003
-#define IDD_OPTIONS_SECTION_4           3003
-#define IDC_EDIT_TARGET_X               3003
-#define IDC_EDIT_TARGET_Y               3004
-#define IDC_EDIT_TARGET_Z               3005
-#define IDC_EDIT_TARGET_X3              3005
-#define IDC_CHECK_TEXTURE_ON            3007
-#define IDR_RENDER                      12006
-#define IDC_BT_MATERIAL_COLOR           19001
-#define IDC_EDIT_TRANSPARENCY           22005
-#define IDC_EDIT_IOR                    22006
-#define IDC_EDIT_TEXTURE_PLACEHOLDER    22007
-#define IDD_COLOR_SECTION               22008
-#define IDC_EDIT_TEXTURE_AMOUNT         22008
-#define IDD_VIEW_PROPERTIES             22009
-#define IDD_MATERIAL_SECTION            22010
-#define IDD_NEW_MATERIAL_SECTION        22011
+#define IDC_BT_MATERIAL_COLOR           3001
+#define IDC_CHECK_EXPERT_MODE           3002
+#define IDC_CHECK_TEXTURE_ON            3003
+#define IDC_CHECK1                      3004
+#define IDC_CHECK2                      3005
+#define IDC_EDIT_IOR                    3006
+#define IDC_EDIT_TARGET_X               3007
+#define IDC_EDIT_TARGET_X3              3008
+#define IDC_EDIT_TARGET_Y               3009
+#define IDC_EDIT_TARGET_Z               3010
+#define IDC_EDIT_TEXTURE_AMOUNT         3011
+#define IDC_EDIT_TEXTURE_PLACEHOLDER    3012
+#define IDC_EDIT_TRANSPARENCY           3013
+#define IDD_COLOR_SECTION               3014
+#define IDD_MATERIAL_SECTION            3015
+#define IDD_NEW_MATERIAL_SECTION        3016
+#define IDD_OPTIONS_SECTION_1           3017
+#define IDD_OPTIONS_SECTION_2           3018
+#define IDD_OPTIONS_SECTION_4           3019
+#define IDD_VIEW_PROPERTIES             3020
 #define ID_APP_VIEW_NORMALVIEW          32777
 #define ID_APP_VIEW_IN2X                32779
 #define ID_APP_VIEW_IN3X                32780
@@ -54,9 +53,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        3010
+#define _APS_NEXT_RESOURCE_VALUE        3021
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         3008
-#define _APS_NEXT_SYMED_VALUE           3000
+#define _APS_NEXT_CONTROL_VALUE         3021
+#define _APS_NEXT_SYMED_VALUE           3021
 #endif
 #endif
