98bb4477069d7f297ba4840fbe177f7e70b02967
Dale Fugier <dale@mcneel.com>
2018-05-14 14:24:30 -0700

    Updated component info

--- grasshopper/cs/SampleGhTaskCapable/SampleGhTaskCapableInfo.cs
+++ grasshopper/cs/SampleGhTaskCapable/SampleGhTaskCapableInfo.cs
@@ -7,46 +7,10 @@ namespace SampleGhTaskCapable
   public class SampleGhTaskCapableInfo : GH_AssemblyInfo
   {
     public override string Name => "SampleGhTaskCapable";
-
-    public override Bitmap Icon
-    {
-      get
-      {
-        //Return a 24x24 pixel bitmap to represent this GHA library.
-        return null;
-      }
-    }
-    public override string Description
-    {
-      get
-      {
-        //Return a short string describing the purpose of this GHA library.
-        return "";
-      }
-    }
-    public override Guid Id
-    {
-      get
-      {
-        return new Guid("e0332029-ae49-484c-8ceb-be9b29636df1");
-      }
-    }
-
-    public override string AuthorName
-    {
-      get
-      {
-        //Return a string identifying you or your company.
-        return "";
-      }
-    }
-    public override string AuthorContact
-    {
-      get
-      {
-        //Return a string representing your preferred contact details.
-        return "";
-      }
-    }
+    public override Bitmap Icon => Properties.Resources.SampleGhTaskCapableComponent_24x24;
+    public override string Description => "Sample Grasshopper component for Rhinoceros 6";
+    public override Guid Id => new Guid("e0332029-ae49-484c-8ceb-be9b29636df1");
+    public override string AuthorName => "Robert McNeel & Associates";
+    public override string AuthorContact => "devsupport@mcneel.com";
   }
 }
