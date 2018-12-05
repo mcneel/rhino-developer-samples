be80e8155f48bfbe0fcc7382c714b2530e3979d8
Dale Fugier <dale@mcneel.com>
2018-12-05 13:03:28 -0800

    Minor tune up to user data definition

--- rhinocommon/cs/SampleCsUserData/SampleCsUserDataObject.cs
+++ rhinocommon/cs/SampleCsUserData/SampleCsUserDataObject.cs
@@ -1,21 +1,43 @@
 ﻿using Rhino.DocObjects.Custom;
+using Rhino.FileIO;
 
 namespace SampleCsUserData
 {
   /// <summary>
-  /// Simple user data object that can be attached to objects or object attributes.
+  /// Sample user data object that can be attached to geometry or object attributes.
   /// </summary>
   [System.Runtime.InteropServices.Guid("3A84A0E6-3EB8-4737-AE2F-4C340A2A11CD")]
   public class SampleCsUserDataObject : UserData
   {
-    public string Notes
-    {
-      get;
-      set;
-    }
+    #region Private constants
 
+    /// <summary>
+    /// The major and minor version number of this data.
+    /// </summary>
+    private const int MAJOR_VERSION = 1;
+    private const int MINOR_VERSION = 0;
+
+    #endregion
+
+    #region Public properties
+
+    /// <summary>
+    /// The notes field
+    /// </summary>
+    public string Notes { get; set; }
+
+    /// <summary>
+    /// Returns true of our user data is valid.
+    /// </summary>
     public bool IsValid => !string.IsNullOrEmpty(Notes);
 
+    #endregion
+
+    #region Userdata overrides
+
+    /// <summary>
+    /// Descriptive name of the user data.
+    /// </summary>
     public override string Description => "SampleCsUserData";
 
     public override string ToString()
@@ -23,6 +45,9 @@ namespace SampleCsUserData
       return Description;
     }
 
+    /// <summary>
+    /// Is called when the object is being duplicated.
+    /// </summary>
     protected override void OnDuplicate(UserData source)
     {
       if (source is SampleCsUserDataObject src)
@@ -31,21 +56,52 @@ namespace SampleCsUserData
       }
     }
 
+    /// <summary>
+    /// If you want to save this user data in a 3dm file, override ShouldWrite and return
+    /// true. If you do support serialization, you must also override the Read and Write
+    /// functions.
+    /// </summary>
     public override bool ShouldWrite => IsValid;
 
-    protected override bool Read(Rhino.FileIO.BinaryArchiveReader archive)
+    /// <summary>
+    /// Reads the content of this data from a stream archive.
+    /// </summary>
+    protected override bool Read(BinaryArchiveReader archive)
     {
+      // Read the chuck version
       archive.Read3dmChunkVersion(out var major, out var minor);
-      if (1 == major && 0 == minor)
-        Notes = archive.ReadString();
+      if (major == MAJOR_VERSION)
+      {
+        // Read 1.0 fields  here
+        if (minor >= MINOR_VERSION)
+        {
+          Notes = archive.ReadString();
+        }
+
+        // Note, if you every roll the minor version number,
+        // then read those fields here.
+      }
+
       return !archive.ReadErrorOccured;
     }
 
-    protected override bool Write(Rhino.FileIO.BinaryArchiveWriter archive)
+    /// <summary>
+    /// Writes the content of this data to a stream archive.
+    /// </summary>
+    protected override bool Write(BinaryArchiveWriter archive)
     {
-      archive.Write3dmChunkVersion(1, 0);
+      // Write the chuck version
+      archive.Write3dmChunkVersion(MAJOR_VERSION, MINOR_VERSION);
+
+      // Write 1.0 fields
       archive.WriteString(Notes);
+
+      // Note, if you every roll the minor version number,
+      // then write those fields here.
+
       return !archive.WriteErrorOccured;
     }
+
+    #endregion
   }
 }
