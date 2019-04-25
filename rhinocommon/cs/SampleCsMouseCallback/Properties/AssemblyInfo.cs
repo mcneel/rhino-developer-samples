fe1cc3a1c8d817660938497e1789f6b60c90a6da
Nathan Letwory <nathan@mcneel.com>
2019-04-25 15:37:28 +0300

    Add a MouseCallback sample.

---
+++ rhinocommon/cs/SampleCsMouseCallback/Properties/AssemblyInfo.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional.
+// These will show in Rhino's option dialog, in the tab Plug-ins.
+[assembly: PlugInDescription(DescriptionType.Address, "-")]
+[assembly: PlugInDescription(DescriptionType.Country, "-")]
+[assembly: PlugInDescription(DescriptionType.Email, "-")]
+[assembly: PlugInDescription(DescriptionType.Phone, "-")]
+[assembly: PlugInDescription(DescriptionType.Fax, "-")]
+[assembly: PlugInDescription(DescriptionType.Organization, "-")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "-")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "-")]
+
+// Icons should be Windows .ico files and contain 32-bit images in the following sizes: 16, 24, 32, 48, and 256.
+// This is a Rhino 6-only description.
+[assembly: PlugInDescription(DescriptionType.Icon, "SampleCsMouseCallback.EmbeddedResources.plugin-utility.ico")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleCsMouseCallback")]
+
+// This will be used also for the plug-in description.
+[assembly: AssemblyDescription("SampleCsMouseCallback utility plug-in")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleCsMouseCallback")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf2a3245-3eb5-4247-8532-a09139eec262")] // This will also be the Guid of the Rhino plug-in
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Make compatible with Rhino Installer Engine
+[assembly: AssemblyInformationalVersion("2")]
---
+++ rhinocommon/cs/SampleCsMouseCallback/SampleCsMouseCallbackCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace SampleCsMouseCallback
+{
+	public class SampleCsMouseCallbackCommand : Command
+	{
+		readonly SampleMouseCallback smcb = new SampleMouseCallback();
+		public SampleCsMouseCallbackCommand()
+		{
+			// Rhino only creates one instance of each command class defined in a
+			// plug-in, so it is safe to store a refence in a static property.
+			Instance = this;
+		}
+
+		///<summary>The only instance of this command.</summary>
+		public static SampleCsMouseCallbackCommand Instance
+		{
+			get; private set;
+		}
+
+		///<returns>The command name as it appears on the Rhino command line.</returns>
+		public override string EnglishName
+		{
+			get { return "SampleCsMouseCallbackCommand"; }
+		}
+
+		protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+		{
+			smcb.Enabled = !smcb.Enabled;
+			return Result.Success;
+		}
+	}
+}
