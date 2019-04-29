a1a230b9a312e826b13af7238d50cad8a301276b
Dale Fugier <dale@mcneel.com>
2019-04-29 10:50:30 -0700

    Added SampleAddTextObject

--- cpp/SampleCommands/cmdSampleAddText.cpp
+++ cpp/SampleCommands/cmdSampleAddText.cpp
@@ -1,67 +1,6 @@
 #include "stdafx.h"
 
-////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-//
-// BEGIN SampleAddText command
-//
-
-#pragma region SampleAddText command
-
-class CCommandSampleAddText : public CRhinoCommand
-{
-public:
-  CCommandSampleAddText() = default;
-  UUID CommandUUID() override
-  {
-    // {4C5D737C-7BB3-4152-A866-D062F7518ABA}
-    static const GUID SampleAddTextCommand_UUID =
-    { 0x4C5D737C, 0x7BB3, 0x4152,{ 0xA8, 0x66, 0xD0, 0x62, 0xF7, 0x51, 0x8A, 0xBA } };
-    return SampleAddTextCommand_UUID;
-  }
-  const wchar_t* EnglishCommandName() override { return L"SampleAddText"; }
-  CRhinoCommand::result RunCommand(const CRhinoCommandContext& context) override;
-
-private:
-  CRhinoText* CreateText(
-    CRhinoDoc& doc,
-    const ON_3dPoint& pt,
-    const wchar_t* text,
-    const wchar_t* font_name,
-    double height,
-    int style,
-    double rotate,
-    ON::TextVerticalAlignment valign,
-    ON::TextHorizontalAlignment halign
-  );
-
-};
-
-// The one and only CCommandSampleAddText object
-static class CCommandSampleAddText theSampleAddTextCommand;
-
-CRhinoCommand::result CCommandSampleAddText::RunCommand(const CRhinoCommandContext& context)
-{
-  CRhinoGetPoint gp;
-  gp.SetCommandPrompt(L"Text location");
-  gp.GetPoint();
-  if (gp.CommandResult() != CRhinoCommand::success)
-    return gp.CommandResult();
-
-  ON_3dPoint pt = gp.Point();
-  ON_wString text = L"Hello Rhino!";
-
-  CRhinoText* text_obj = CreateText(context.m_doc, pt, text, L"Times New Roman", 1.0, 0, 0.0, ON::TextVerticalAlignment::Bottom, ON::TextHorizontalAlignment::Left);
-  if (nullptr != text_obj)
-  {
-    context.m_doc.AddObject(text_obj);
-    context.m_doc.Redraw();
-  }
-
-  return CRhinoCommand::success;
-}
-
-CRhinoText* CCommandSampleAddText::CreateText(
+CRhinoText* RhinoCreateText(
   CRhinoDoc& doc,
   const ON_3dPoint& pt,
   const wchar_t* text,
@@ -92,7 +31,7 @@ CRhinoText* CCommandSampleAddText::CreateText(
     return rc;
 
   height = fabs(height);
-  
+
   ON_Plane plane = RhinoActiveCPlane();
   plane.Rotate(rotate, plane.zaxis);
 
@@ -118,10 +57,153 @@ CRhinoText* CCommandSampleAddText::CreateText(
 }
 
 
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
+// BEGIN SampleAddText command
+//
+
+#pragma region SampleAddText command
+
+class CCommandSampleAddText : public CRhinoCommand
+{
+public:
+  CCommandSampleAddText() = default;
+  UUID CommandUUID() override
+  {
+    // {4C5D737C-7BB3-4152-A866-D062F7518ABA}
+    static const GUID SampleAddTextCommand_UUID =
+    { 0x4C5D737C, 0x7BB3, 0x4152,{ 0xA8, 0x66, 0xD0, 0x62, 0xF7, 0x51, 0x8A, 0xBA } };
+    return SampleAddTextCommand_UUID;
+  }
+  const wchar_t* EnglishCommandName() override { return L"SampleAddText"; }
+  CRhinoCommand::result RunCommand(const CRhinoCommandContext& context) override;
+};
+
+// The one and only CCommandSampleAddText object
+static class CCommandSampleAddText theSampleAddTextCommand;
+
+CRhinoCommand::result CCommandSampleAddText::RunCommand(const CRhinoCommandContext& context)
+{
+  CRhinoGetPoint gp;
+  gp.SetCommandPrompt(L"Text location");
+  gp.GetPoint();
+  if (gp.CommandResult() != CRhinoCommand::success)
+    return gp.CommandResult();
+
+  ON_3dPoint pt = gp.Point();
+  ON_wString text = L"Hello Rhino!";
+
+  CRhinoText* text_obj = RhinoCreateText(context.m_doc, pt, text, L"Times New Roman", 1.0, 0, 0.0, ON::TextVerticalAlignment::Bottom, ON::TextHorizontalAlignment::Left);
+  if (nullptr != text_obj)
+  {
+    context.m_doc.AddObject(text_obj);
+    context.m_doc.Redraw();
+  }
+
+  return CRhinoCommand::success;
+}
+
 #pragma endregion
 
 //
 // END SampleAddText command
+//
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
+// BEGIN SampleAddTextObject command
+//
+
+#pragma region SampleAddTextObject command
+
+class CCommandSampleAddTextObject : public CRhinoCommand
+{
+public:
+  CCommandSampleAddTextObject() = default;
+  UUID CommandUUID() override
+  {
+    // {87743F94-5612-4855-BD0C-59CD91148785}
+    static const GUID SampleAddTextObjectCommand_UUID =
+    { 0x87743F94, 0x5612, 0x4855, { 0xBD, 0x0C, 0x59, 0xCD, 0x91, 0x14, 0x87, 0x85 } };
+    return SampleAddTextObjectCommand_UUID;
+  }
+  const wchar_t* EnglishCommandName() override { return L"SampleAddTextObject"; }
+  CRhinoCommand::result RunCommand(const CRhinoCommandContext& context) override;
+};
+
+// The one and only CCommandSampleAddTextObject object
+static class CCommandSampleAddTextObject theSampleAddTextObjectCommand;
+
+CRhinoCommand::result CCommandSampleAddTextObject::RunCommand(const CRhinoCommandContext& context)
+{
+  CRhinoGetPoint gp;
+  gp.SetCommandPrompt(L"Text object location");
+  gp.GetPoint();
+  if (gp.CommandResult() != CRhinoCommand::success)
+    return gp.CommandResult();
+
+  CRhinoView* view = gp.View();
+  if (nullptr == view)
+    return CRhinoCommand::failure;
+
+  ON_3dPoint pt = gp.Point();
+  ON_wString text = L"Hello Rhino!";
+
+  CRhinoText* text_obj = RhinoCreateText(context.m_doc, pt, text, L"Times New Roman", 1.0, 0, 0.0, ON::TextVerticalAlignment::Bottom, ON::TextHorizontalAlignment::Left);
+  if (nullptr == text_obj)
+    return CRhinoCommand::failure;
+
+  const ON_Annotation* annotation = text_obj->Annotation();
+  if (nullptr == annotation)
+    return CRhinoCommand::failure;
+
+  const ON_DimStyle* dim_style = &annotation->DimensionStyle(
+    context.m_doc.m_dimstyle_table[context.m_doc.m_dimstyle_table.FindDimStyleFromId(annotation->DimensionStyleId(),
+    true,
+    true,
+    context.m_doc.m_dimstyle_table.CurrentDimStyleIndex())]);
+
+  ON_SimpleArray<ON_Object*> output;
+  bool rc = RhinoCreateTextObjectGeometry(
+    annotation,
+    &view->Viewport().VP(),
+    dim_style,
+    1.0,
+    true,
+    context.m_doc.AbsoluteTolerance(),
+    1.0,
+    ON::object_type::extrusion_object,
+    1.0,
+    0.0,
+    output
+  );
+  if (!rc)
+    return CRhinoCommand::failure;
+
+  for (int i = 0; i < output.Count(); i++)
+  {
+    if (nullptr != output[i])
+    {
+      CRhinoExtrusionObject* extrusion_obj = new CRhinoExtrusionObject();
+      extrusion_obj->SetExtrusion(ON_Extrusion::Cast(output[i]));
+      context.m_doc.AddObject(extrusion_obj);
+    }
+  }
+
+  context.m_doc.Redraw();
+
+  return CRhinoCommand::success;
+}
+
+#pragma endregion
+
+//
+// END SampleAddTextObject command
 //
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
