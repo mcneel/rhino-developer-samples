1fc69b35b9aa5a136fe0fb51ad7ffbf919ee161a
Dale Fugier <dale@mcneel.com>
2019-05-14 09:42:54 -0700

    Added SampleCsLayerOffInDetails command

--- rhinocommon/cs/SampleCsCommands/SampleCsLayerOff.cs
+++ rhinocommon/cs/SampleCsCommands/SampleCsLayerOff.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows.Forms;
+﻿using System.Linq;
 using Rhino;
 using Rhino.Commands;
 
@@ -6,10 +6,7 @@ namespace SampleCsCommands
 {
   public class SampleCsLayerOff : Command
   {
-    public override string EnglishName
-    {
-      get { return "SampleCsLayerOff"; }
-    }
+    public override string EnglishName => "SampleCsLayerOff";
 
     protected override Result RunCommand(RhinoDoc doc, RunMode mode)
     {
@@ -30,6 +27,62 @@ namespace SampleCsCommands
       layer.SetPersistentVisibility(false);
 
       doc.Views.Redraw();
+
+      return Result.Success;
+    }
+  }
+
+  public class SampleCsLayerOffInDetails : Command
+  {
+    public override string EnglishName => "SampleCsLayerOffInDetails";
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      // Find the layer index by full path name
+      var layer_index = doc.Layers.FindByFullPath("TO-DRAW", -1);
+      if (-1 == layer_index)
+        return Result.Cancel;
+
+      // Get the layer object
+      var layer = doc.Layers[layer_index];
+      if (null == layer)
+        return Result.Failure;
+
+      // If the layer is off globally, then there is nothing to do
+      if (!layer.IsVisible)
+        return Result.Cancel;
+
+      // Find the page view
+      var page_view = doc.Views.GetPageViews().First(
+        item => item.PageName.Equals("PANEL_01", System.StringComparison.OrdinalIgnoreCase)
+        );
+
+      if (null == page_view)
+        return Result.Cancel;
+
+      // Process each detail
+      foreach (var detail in page_view.GetDetailViews())
+      {
+        var viewport_id = detail.Viewport.Id;
+
+        // Re-acquire the layer object, as the underlying
+        // object may have been modified
+        layer = doc.Layers[layer_index];
+        if (null != layer)
+        {
+          // If the layer is visible on the detail, turn it off
+          if (layer.PerViewportIsVisible(viewport_id))
+          {
+            layer.SetPerViewportVisible(viewport_id, false);
+            layer.SetPerViewportPersistentVisibility(viewport_id, false);
+          }
+        }
+      }
+
+      // Redraw if needed
+      var view = doc.Views.ActiveView;
+      if (null != view && view.ActiveViewportID == page_view.ActiveViewportID)
+        doc.Views.Redraw();
 
       return Result.Success;
     }
