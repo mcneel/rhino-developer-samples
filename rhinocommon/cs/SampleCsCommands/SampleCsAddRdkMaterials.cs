12cc83b31c32ad04c3273d62cc48b364bd4427bc
Dale Fugier <dale@mcneel.com>
2020-07-10 14:47:03 -0700

    Minor cleanup of comments.

--- rhinocommon/cs/SampleCsCommands/SampleCsAddRdkMaterials.cs
+++ rhinocommon/cs/SampleCsCommands/SampleCsAddRdkMaterials.cs
@@ -13,13 +13,14 @@ namespace SampleCsCommands
 
     protected override Result RunCommand(RhinoDoc doc, RunMode mode)
     {
-      // A properties of a Rhino material define what is required to draw an
-      // object using OpenGL. Most rendering plug-ins require a much richer
-      // defintion than this. And, not all rendering  plug-ins support the
-      // same material properties. This is where Render materials come in. 
-      // An Render material is an abstract material - one that is defined 
-      // by the the owning render plug-in. But a basic Render material does
-      // share some properties with a Rhino material.
+      // The properties of a Rhino material (Rhino.DocObjects.Material) define
+      // what is required to draw an object using OpenGL. Most rendering plug-ins
+      // require a much richer definition than this. And, not all rendering
+      // plug-ins support the same material properties. This is where Render
+      // materials (Rhino.Render.RenderMaterial) come in. A Render material
+      // is an abstract material - one that is defined by the the owning 
+      // rendering plug-in. But a basic Render material does share some
+      // properties with a Rhino material.
 
       // This example demonstrates how to create a basic Render material.
       // This material will appear in Rhino's Materials panel. It it called
@@ -50,17 +51,17 @@ namespace SampleCsCommands
       var green_render_material = RenderMaterial.CreateBasicMaterial(green_material);
       var blue_render_material = RenderMaterial.CreateBasicMaterial(blue_material);
 
-      // 3.) Add basic Render materials to the RDK document
+      // 3.) Add basic Render materials to the render material table
       doc.RenderMaterials.Add(red_render_material);
       doc.RenderMaterials.Add(green_render_material);
       doc.RenderMaterials.Add(blue_render_material);
-      
+
       // At this point, you will see render materials show up in Rhino's
-      // Materials panel. Note, doc.Materials.Count will equal 0. This is
+      // Materials panel. Note, RhinoDoc.Materials.Count will equal 0. This is
       // because we have not added any Rhino materials. We've only added
       // Render materials. When you assign a Render material to an object
-      // or a layer, a compatible Rhino material will be added doc.Materials,
-      // and this materiall will be referenced by the Render material.
+      // or a layer, a compatible Rhino material will be added to RhinoDoc.Materials,
+      // and this material will be referenced by the Render material.
 
       // Note, it is possible to add Rhino materials to the document. However,
       // they will not appear in the Materials panel until they are assigned 
