d0619bec4974d800544f22e182f06abc545fd6b0
sodermax <maxsoderstrom@gmail.com>
2020-10-05 16:28:45 +0300

    Add ShouldDisplay&Update page to viewport sample page

--- rhinocommon/cs/SampleCsViewportPropertiesETOUI/ViewportPropertiesPage.cs
+++ rhinocommon/cs/SampleCsViewportPropertiesETOUI/ViewportPropertiesPage.cs
@@ -5,30 +5,79 @@ using Rhino.UI.Controls;
 using Eto.Forms;
 using Rhino.UI;
 using System.Drawing;
+using Rhino.Display;
+using Rhino.DocObjects;
 
 namespace SampleViewportPropertiesETOUI
 {
-  public class ViewportPropertiesPage : ObjectPropertiesPage
-  {
-    private uint[] m_selected_serial_numbers = { };
-
-    public ViewportPropertiesPage()
+    public class ViewportPropertiesPage : ObjectPropertiesPage
     {
-      CollapsibleSectionHolder = new CollapsibleSectionUIPanel();
+
+        public ViewportPropertiesPage()
+        {
+
+        }
+
+        public override string EnglishPageTitle => "Viewport";
+
+        // If no icon then the Title will be displayed in the Tab
+        public override System.Drawing.Icon PageIcon(Size size)
+        {
+            return null;
+        }
+
+        public override PropertyPageType PageType => PropertyPageType.View;
+
+        public sealed override object PageControl
+        {
+            get
+            {
+                if (CollapsibleSectionHolder == null)
+                {
+                    CollapsibleSectionHolder = NewPanel();
+                }
+                return CollapsibleSectionHolder;
+            }
+        }
+
+        public override bool ShouldDisplay(ObjectPropertiesPageEventArgs e)
+        {
+            if (e.View == null)
+            {
+                if (CollapsibleSectionHolder == null)
+                    return false;
+
+                // Update page here and tell it no view
+                // is selected.
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void UpdatePage(ObjectPropertiesPageEventArgs e)
+        {
+            if (CollapsibleSectionHolder == null)
+                return;
+
+            ViewInfo vi = new ViewInfo(e.Viewport);
+
+            if (vi != null)
+            {
+                // Pass the view info to the page
+            }
+            else
+            {
+                // Tell the ui that there is no view.
+            }
+        }
+
+        private uint[] m_selected_serial_numbers = { };
+        private CollapsibleSectionUIPanel m_page;
+        private CollapsibleSectionUIPanel CollapsibleSectionHolder { get { return m_page; } set { m_page = value; } }
+
+        private CollapsibleSectionUIPanel NewPanel() => new CollapsibleSectionUIPanel();
     }
-
-    public override string EnglishPageTitle => "Viewport";
-
-    // If no icon then the Title will be displayed in the Tab
-    public override System.Drawing.Icon PageIcon(Size size)
-    {
-      return null;
-    }
-
-    public override PropertyPageType PageType => PropertyPageType.View;
-
-    public sealed override object PageControl => CollapsibleSectionHolder;
-
-    private CollapsibleSectionUIPanel CollapsibleSectionHolder { get; }
-  }
 }
+
