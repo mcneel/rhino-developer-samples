74a65e4aa79f554d5b667e087978be21733437eb
Luis Fraguada <luis@mcneel.com>
2020-12-09 11:06:41 +0100

    updating python compute sample, tested on py 3.8

--- compute/py/SampleTkinter/makemesh.py
+++ compute/py/SampleTkinter/makemesh.py
@@ -4,8 +4,8 @@ import time
 import compute_rhino3d.Mesh
 import compute_rhino3d.Util
 
-
-top = Tkinter.Tk()
+compute_rhino3d.Util.url = "http://localhost:8081/"
+#compute_rhino3d.Util.apiKey = ""
 
 center = Point3d(250, 250, 0)
 sphere = Sphere(center, 100)
