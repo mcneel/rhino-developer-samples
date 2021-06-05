3018a9f40adbbc066de6b3f4e13104501916395b
Uğur Özcan <ugurozcan108@gmail.com>
2021-06-05 16:17:56 +0300

    write circles into 3dm file

---
+++ rhino3dm/py/SampleCreateCircleAndWriteTo3dmFile.py
@@ -0,0 +1,11 @@
+import rhino3dm
+import random
+
+model = rhino3dm.File3dm()
+for i in range(20):
+	pt = rhino3dm.Point3d(random.uniform(-10,10), random.uniform(-10,10), 0)
+	model.Objects.AddPoint(pt)
+	circle = rhino3dm.Circle(pt, random.uniform(1,4))
+	model.Objects.AddCircle(circle)
+	
+model.Write("circle.3dm", 6)
