2242aa05dc9bf3dca59d4295cd29a7bb57f6f09e
Andrew le Bihan <andy@mcneel.com>
2022-01-30 17:17:16 +0200

    Clean up SampleRdkAddRdkMaterials - also adds an extra method

--- cpp/SampleRdkAddRdkMaterials/SampleRdkAddRdkMaterialsCommand.cpp
+++ cpp/SampleRdkAddRdkMaterials/SampleRdkAddRdkMaterialsCommand.cpp
@@ -10,47 +10,31 @@ protected:
 }
 theSampleRdkAddRdkMaterialsCmd;
 
-CRhinoCommand::result CSampleRdkAddRdkMaterialsCmd::RunCommand(const CRhinoCommandContext& context)
+static void AttachTestBitmapTextureToMaterial(const CRhinoDoc* pDocAssoc, CRhRdkMaterial& material, const wchar_t* wszChildSlotName)
 {
-  auto* pDoc = context.Document();
-  if (nullptr == pDoc)
-    return failure;
+    // Add a bitmap texture to the bitmap slot of the material.
+    CRhRdkSimulatedTexture tex(pDocAssoc);
+    tex.SetFilename(L"C:\\example_image.jpg", pDocAssoc, false); // TODO: Set the full path to the file.
+    auto* pBitmap = ::RhRdkNewBitmapTexture(tex, pDocAssoc);
+    if (nullptr != pBitmap)
+    {
+        material.SetChild(pBitmap, wszChildSlotName);
+        material.SetChildSlotOn(wszChildSlotName, true);
+    }
+}
 
-  CRhRdkMaterial* pCustom = nullptr;
-  
+static void AttachTestWoodTextureToMaterial(const CRhinoDoc* pDocAssoc, CRhRdkMaterial& material, const wchar_t* wszChildSlotName)
+{
+    auto* pWood = ::RhRdkContentFactoriesEx().NewContentFromTypeEx(uuidWoodTextureType, pDocAssoc);
+    if (nullptr != pWood)
+    {
+        material.SetChild(pWood, wszChildSlotName);
+        material.SetChildSlotOn(wszChildSlotName, true);
+    }
+}
 
-  {{{
-
-  // Create an ON_Material.
-  ON_Material mat;
-  ON_Color col(255, 0, 0); // Initial color.
-  mat.SetDiffuse(col);
-  mat.SetName(L"Sample Custom");
-
-  // Create a Basic (Custom) Material using the ON_Material.
-  pCustom = ::RhRdkNewBasicMaterial(mat, pDoc);
-  if (nullptr == pCustom)
-    return failure;
-
-  // Add a bitmap texture to the bitmap slot of the Basic Material.
-  CRhRdkSimulatedTexture tex(pDoc);
-  tex.SetFilename(L"C:\\example_image.jpg", pDoc, false); // TODO: Set the full path to the file.
-  auto* pBitmap = ::RhRdkNewBitmapTexture(tex, pDoc);
-  if (nullptr != pBitmap)
-  {
-      pCustom->SetChild(pBitmap, CS_MAT_BITMAP_TEXTURE);
-      pCustom->SetChildSlotOn(CS_MAT_BITMAP_TEXTURE, true);
-  }
-
-  // Add a wood texture to the transparency slot of the Basic Material.
-  auto* pWood = ::RhRdkContentFactoriesEx().NewContentFromTypeEx(uuidWoodTextureType, pDoc);
-  if (nullptr != pWood)
-  {
-      pCustom->SetChild(pWood, CS_MAT_TRANSPARENCY_TEXTURE);
-      pCustom->SetChildSlotOn(CS_MAT_TRANSPARENCY_TEXTURE, true);
-  }
-
-  // This section is optional and demonstrates how to add a dib texture to the bump slot of the Basic Material.
+static void AttachTestRuntimeDibTextureToMaterial(const CRhinoDoc* pDocAssoc, CRhRdkMaterial& material, const wchar_t* wszChildSlotName)
+{
     const int width = 320, height = 240, depth = 32;
     auto dib = std::make_shared<CRhinoDib>(width, height, depth);
 
@@ -64,60 +48,97 @@ CRhinoCommand::result CSampleRdkAddRdkMaterialsCmd::RunCommand(const CRhinoComma
     dib->ProcessPixels(func);
 
     // Create a dib texture from the dib and set it as a child of the Basic Material in the bump texture slot.
-    auto* pTexture = ::RhRdkNewDibTexture(dib, pDoc);
+    auto* pTexture = ::RhRdkNewDibTexture(dib, pDocAssoc);
     if (nullptr != pTexture)
     {
-        pCustom->SetChild(pTexture, CS_MAT_BUMP_TEXTURE);
-        pCustom->SetChildSlotOn(CS_MAT_BUMP_TEXTURE, true);
+        material.SetChild(pTexture, wszChildSlotName);
+        material.SetChildSlotOn(wszChildSlotName, true);
     }
+}
 
-  }}}
+CRhinoCommand::result CSampleRdkAddRdkMaterialsCmd::RunCommand(const CRhinoCommandContext& context)
+{
+  auto* pDoc = context.Document();
+  if (nullptr == pDoc)
+    return failure;
 
-  CRhRdkMaterial* pPBR = nullptr;
+  ON_SimpleArray<CRhRdkMaterial*> materials;
 
-  {{{
-
-  //Now do the same for a PBR material.
-  // Create an ON_Material as a PBR.
-  ON_Material mat;
-  mat.SetName(L"Sample PBR");
-
-  mat.ToPhysicallyBased();
-  ASSERT(mat.IsPhysicallyBased());
-  auto pbr = mat.PhysicallyBased();
-
-  ON_4fColor col;
-  col.SetRGBA(1.f, 0.f, 0.f, 1.f);
-  pbr->SetBaseColor(col);
-
-  pPBR = CRhRdkMaterial::FromOnMaterial(mat, pDoc);
-
-  // Add a bitmap texture to the bitmap slot of the Basic Material.
-  CRhRdkSimulatedTexture tex(pDoc);
-  tex.SetFilename(L"C:\\example_image.jpg", pDoc, false); // TODO: Set the full path to the file.
-  auto* pBitmap = ::RhRdkNewBitmapTexture(tex, pDoc);
-  if (nullptr != pBitmap)
+  //METHOD 1:
+  //Create a custom material from an ON_Material
   {
-      pPBR->SetChild(pBitmap, CS_MAT_PBR_BASE_COLOR);
-      pPBR->SetChildSlotOn(CS_MAT_PBR_BASE_COLOR, true);
+      // Create an ON_Material.
+      ON_Material mat;
+      ON_Color col(255, 0, 0); // Initial color.
+      mat.SetDiffuse(col);
+
+      // Create a Basic (Custom) Material using the ON_Material.
+      auto* pCustom = CRhRdkMaterial::FromOnMaterial(mat, pDoc);
+      if (nullptr == pCustom)
+        return failure;
+
+      pCustom->SetInstanceName(L"#1: Sample Custom");
+
+      AttachTestBitmapTextureToMaterial(pDoc, *pCustom, CS_MAT_BITMAP_TEXTURE);
+      AttachTestWoodTextureToMaterial(pDoc, *pCustom, CS_MAT_TRANSPARENCY_TEXTURE);
+      AttachTestRuntimeDibTextureToMaterial(pDoc, *pCustom, CS_MAT_BUMP_TEXTURE);
+
+      materials.Append(pCustom);
   }
 
-  // Add a wood texture to the transparency slot of the Basic Material.
-  auto* pWood = ::RhRdkContentFactoriesEx().NewContentFromTypeEx(uuidWoodTextureType, pDoc);
-  if (nullptr != pWood)
+  //METHOD 2:
+  //Create a PBR material from an ON_Material set into PBR mode.
   {
-      pPBR->SetChild(pWood, CS_MAT_PBR_ROUGHNESS);
-      pPBR->SetChildSlotOn(CS_MAT_PBR_ROUGHNESS, true);
+      //Now do the same for a PBR material.
+      // Create an ON_Material as a PBR.
+      ON_Material mat;
+
+      mat.ToPhysicallyBased();
+      ASSERT(mat.IsPhysicallyBased());
+      auto pbr = mat.PhysicallyBased();
+
+      ON_4fColor col;
+      col.SetRGBA(1.f, 0.f, 0.f, 1.f);
+      pbr->SetBaseColor(col);
+
+      auto* pPBR = CRhRdkMaterial::FromOnMaterial(mat, pDoc);
+
+      pPBR->SetInstanceName(L"#2: Sample PBR");
+
+      AttachTestBitmapTextureToMaterial(pDoc, *pPBR, CS_MAT_PBR_BASE_COLOR);
+      AttachTestWoodTextureToMaterial(pDoc, *pPBR, CS_MAT_PBR_OPACITY);
+      AttachTestRuntimeDibTextureToMaterial(pDoc, *pPBR, CS_MAT_PBR_METALLIC);
+
+      materials.Append(pPBR);
   }
 
-  }}}
+  //METHOD 3:
+  //Create a PBR material from scratch and fiddle with the parameters directly.
+  {
+      auto* pPBR = static_cast<CRhRdkMaterial*>(::RhRdkContentFactoriesEx().NewContentFromTypeEx(uuidPhysicallyBasedMaterialType, pDoc));
 
-  
+      ON_4fColor col;
+      col.SetRGBA(0.f, 0.f, 1.f, 1.f);
+
+      pPBR->SetParameter(FS_MAT_PBR_BASE_COLOR, col);
+      pPBR->SetParameter(FS_MAT_PBR_METALLIC, true);
+
+      pPBR->SetInstanceName(L"#3: Sample PBR Set Param");
+
+      AttachTestBitmapTextureToMaterial(pDoc, *pPBR, CS_MAT_PBR_BASE_COLOR);
+      AttachTestWoodTextureToMaterial(pDoc, *pPBR, CS_MAT_PBR_ROUGHNESS);
+      AttachTestRuntimeDibTextureToMaterial(pDoc, *pPBR, CS_MAT_PBR_CLEARCOAT);
+
+      materials.Append(pPBR);
+  }
+
 
   // Attach the Basic Material and PBR to the document.
   auto& contents = pDoc->Contents().BeginChange(RhRdkChangeContext::Program);
-  contents.Attach(*pCustom);
-  contents.Attach(*pPBR);
+  for (int i=0;i<materials.Count();i++)
+  {
+      contents.Attach(*materials[i]);
+  }
   contents.EndChange();
 
   // Create a sphere object and assign the custom material to it.
@@ -129,7 +150,7 @@ CRhinoCommand::result CSampleRdkAddRdkMaterialsCmd::RunCommand(const CRhinoComma
   if (nullptr != pObject)
   {
     CRhRdkObjectDataAccess da(pObject);
-    da.SetMaterialInstanceId(pCustom->InstanceId());
+    da.SetMaterialInstanceId(materials[0]->InstanceId());
   }
 
   // Do the same for a layer with the PBR material
@@ -139,7 +160,7 @@ CRhinoCommand::result CSampleRdkAddRdkMaterialsCmd::RunCommand(const CRhinoComma
   {
     const auto& layer = pDoc->m_layer_table[index];
     CRhRdkObjectDataAccess da(&layer);
-    da.SetMaterialInstanceId(pPBR->InstanceId());
+    da.SetMaterialInstanceId(materials[1]->InstanceId());
   }
 
   pDoc->DeferredRedraw();
