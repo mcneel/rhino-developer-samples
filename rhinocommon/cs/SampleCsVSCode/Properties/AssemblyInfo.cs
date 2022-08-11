aa2b307585ff1230f0870eb7bb5ceb855310ac76
Curtis Wensley <curtis.wensley@gmail.com>
2022-08-11 12:40:25 -0700

    Add sample plugin that can compile/debug with VS Code on Mac and Windows

---
+++ rhinocommon/cs/SampleCsVSCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional.
+// These will show in Rhino's option dialog, in the tab Plug-ins.
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Fax, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+
+// Icons should be Windows .ico files and contain 32-bit images in the following sizes: 16, 24, 32, 48, and 256.
+[assembly: PlugInDescription(DescriptionType.Icon, "SampleCsVSCode.EmbeddedResources.plugin-utility.ico")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+// This will also be the Guid of the Rhino plug-in
+[assembly: Guid("d1c48cc8-b079-41cd-910d-3ac4348083cb")] 
