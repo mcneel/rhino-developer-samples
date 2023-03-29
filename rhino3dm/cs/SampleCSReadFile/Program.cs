cbf8e1f8948fc7e8141d4744be65a12f42bc166e
fraguada <luis@mcneel.com>
2023-03-29 11:37:26 +0200

    Updating .net 7 sample

--- rhino3dm/cs/SampleCSReadFile/Program.cs
+++ rhino3dm/cs/SampleCSReadFile/Program.cs
@@ -3,7 +3,16 @@ using Rhino.FileIO;
 
 Console.WriteLine("Hello, World!");
 
-//read file
-var file3dm = File3dm.Read("../../../RectifiedArchimedeanSolids8.3dm");
+var path = Directory.GetCurrentDirectory();
+
+File3dm file3dm = new File3dm();
+
+//the path will be different whether you are using dotnet run or debugging
+if(path.Contains("net7.0")){
+    file3dm = File3dm.Read("../../../RectifiedArchimedeanSolids8.3dm");
+} else{
+    file3dm = File3dm.Read("RectifiedArchimedeanSolids8.3dm");
+} 
+
 Console.WriteLine("Number of objects in file {0}", file3dm.Objects.Count);
 
