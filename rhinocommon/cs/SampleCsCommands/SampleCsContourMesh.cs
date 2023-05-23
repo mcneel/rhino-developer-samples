653ea59e16aea189a0dc068f6bd70b96f1c315fc
Dale Fugier <dale@mcneel.com>
2023-05-23 09:56:06 -0700

    Added SampleCsMirror.cs

--- rhinocommon/cs/SampleCsCommands/SampleCsContourMesh.cs
+++ rhinocommon/cs/SampleCsCommands/SampleCsContourMesh.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Rhino;
 using Rhino.Commands;
+using Rhino.DocObjects;
+using Rhino.Geometry;
 
 namespace SampleCsCommands
 {
@@ -10,28 +12,27 @@ namespace SampleCsCommands
 
     protected override Result RunCommand(RhinoDoc doc, RunMode mode)
     {
-      Rhino.DocObjects.ObjectType filter = Rhino.DocObjects.ObjectType.Mesh;
-      Rhino.DocObjects.ObjRef objref = null;
-      Rhino.Commands.Result rc = Rhino.Input.RhinoGet.GetOneObject("Select mesh to contour", false, filter, out objref);
-      if (rc != Rhino.Commands.Result.Success || objref == null)
+      var filter = ObjectType.Mesh;
+      var rc = Rhino.Input.RhinoGet.GetOneObject("Select mesh to contour", false, filter, out var objref);
+      if (rc != Result.Success || objref == null)
         return rc;
 
-      Rhino.Geometry.Mesh mesh = objref.Geometry() as Rhino.Geometry.Mesh;
+      var mesh = objref.Geometry() as Rhino.Geometry.Mesh;
       if (null == mesh)
-        return Rhino.Commands.Result.Failure;
+        return Result.Failure;
 
-      Rhino.Geometry.BoundingBox bbox = mesh.GetBoundingBox(false);
-      Rhino.Geometry.Point3d start_pt = bbox.Corner(true, true, true);
-      Rhino.Geometry.Point3d end_pt = bbox.Corner(false, true, true);
-      double interval = start_pt.DistanceTo(end_pt) / 10;
+      var bbox = mesh.GetBoundingBox(false);
+      var startPt = bbox.Corner(true, true, true);
+      var endPt = bbox.Corner(false, true, true);
+      var interval = startPt.DistanceTo(endPt) / 10;
 
-      Rhino.Geometry.Curve[] curves = Rhino.Geometry.Mesh.CreateContourCurves(mesh, start_pt, end_pt, interval);
+      var curves = Mesh.CreateContourCurves(mesh, startPt, endPt, interval, doc.ModelAbsoluteTolerance);
       if (null != curves && curves.Length > 0)
       {
-        for (int i = 0; i < curves.Length; i++)
-          doc.Objects.AddCurve(curves[i]);
-        doc.Views.Redraw();
+        foreach (var crv in curves)
+          doc.Objects.AddCurve(crv);
       }
+      doc.Views.Redraw();
 
       return Result.Success;
     }
---
+++ rhinocommon/cs/SampleCsCommands/SampleCsMirror.cs
@@ -0,0 +1,103 @@
+﻿using Rhino;
+using Rhino.Collections;
+using Rhino.Commands;
+using Rhino.Display;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace SampleCsCommands
+{
+  /// <summary>
+  /// Inherit from TransformCommand, not Command
+  /// </summary>
+  public class SampleCsMirror : TransformCommand
+  {
+    private bool CopyObjects { get; set; } = false;
+
+    public override string EnglishName => "SampleCsMirror";
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      var list = new TransformObjectList();
+      var rc = SelectObjects("Select objects to mirror", list);
+      if (rc != Result.Success)
+        return rc;
+
+      var copyObjects = CopyObjects;
+      var copyOption = new OptionToggle(copyObjects, "No", "Yes");
+
+      var gp = new GetPoint();
+      gp.SetCommandPrompt("Start of mirror plane");
+      gp.AddOptionToggle("Copy", ref copyOption);
+      while (true)
+      {
+        var res = gp.Get();
+        if (res == GetResult.Option)
+          continue;
+        else if (res != GetResult.Point)
+          return Result.Cancel;
+        break;
+      }
+
+      var origin = gp.Point();
+
+      var gx = new GetMirrorTransform();
+      gx.AddOptionToggle("Copy", ref copyOption);
+      gx.AddTransformObjects(list);
+      gx.ConstrainToConstructionPlane(true);
+      gx.DrawLineFromPoint(origin, true);
+      gx.SetBasePoint(origin, true);
+      gx.SetCommandPrompt("End of mirror plane");
+
+      while (true)
+      {
+        var res = gx.GetXform();
+        if (res == GetResult.Option)
+          continue;
+        else if (res != GetResult.Point)
+          return Result.Cancel;
+        break;
+      }
+
+      CopyObjects = copyOption.CurrentValue;
+
+      var view = gx.View();
+      if (null != view)
+      {
+        var xform = gx.CalculateTransform(view.ActiveViewport, gx.Point());
+        if (xform.IsValid && !xform.IsIdentity)
+        {
+          // Enable auto-history recording when CopyObjects = true
+          TransformObjects(list, xform, CopyObjects, CopyObjects);
+        }
+      }
+
+      doc.Views.Redraw();
+
+      return Result.Success;
+    }
+  }
+
+  /// <summary>
+  /// Dynamic transformation getter
+  /// </summary>
+  internal class GetMirrorTransform : GetTransform
+  {
+    public override Transform CalculateTransform(RhinoViewport viewport, Point3d point)
+    {
+      var rc = Transform.Identity;
+      if (TryGetBasePoint(out var basePoint))
+      {
+        var plane = viewport.ConstructionPlane();
+        var dir = Vector3d.CrossProduct(point - basePoint, plane.Normal);
+        if (!dir.IsTiny())
+        {
+          dir.Unitize();
+          rc = Transform.Mirror(basePoint, dir);
+        }
+      }
+      return rc;
+    }
+  }
+}
