ce6a469e36628bd53526e2698965f03b40883e68
Dale Fugier <dale@mcneel.com>
2023-09-07 17:27:46 -0700

    More C++ project cleanup

--- cpp/SampleSharedXform/SampleSharedXformOwner/Properties/AssemblyInfo.cs
+++ cpp/SampleSharedXform/SampleSharedXformOwner/Properties/AssemblyInfo.cs
@@ -14,19 +14,6 @@ using Rhino.PlugIns;
 [assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://github.com/mcneel/rhino-developer-samples")]
 [assembly: PlugInDescription(DescriptionType.WebSite, "http://www.rhino3d.com/")]
 [assembly: PlugInDescription(DescriptionType.Icon, "SampleSharedXformOwner.Resources.SampleSharedXformOwner.ico")]
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SampleSharedXformOwner")]
-
-// This will be used also for the plug-in description.
-[assembly: AssemblyDescription("SampleSharedXformOwner utility plug-in")]
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Robert McNeel & Associates")]
-[assembly: AssemblyProduct("SampleSharedXformOwner")]
-[assembly: AssemblyCopyright("Copyright © 2019, Robert McNeel & Associates")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -37,20 +24,3 @@ using Rhino.PlugIns;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1e1cb813-3331-43c0-8e9a-e26525c25134")] // This will also be the Guid of the Rhino plug-in
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-// Make compatible with Rhino Installer Engine
-[assembly: AssemblyInformationalVersion("2")]
