ea258d42f942e7e7fc8fce1b6085e3ac39429f16
Luis Fraguada <luis@mcneel.com>
2023-09-21 15:56:35 +0200

    update 7 compute py samples

--- compute/py/SampleGhBasic/lines.py
+++ compute/py/SampleGhBasic/lines.py
@@ -3,7 +3,7 @@ import compute_rhino3d.Grasshopper as gh
 import rhino3dm
 import json
 
-compute_rhino3d.Util.url = "http://localhost:8081/"
+compute_rhino3d.Util.url = "http://localhost:5000/"
 #compute_rhino3d.Util.apiKey = ""
 
 pt1 = rhino3dm.Point3d(0, 0, 0)
@@ -24,7 +24,7 @@ output = gh.EvaluateDefinition('twisty.gh', trees)
 # print(output)
 
 # decode results
-branch = output['values'][0]['InnerTree']['{ 0; }']
+branch = output['values'][0]['InnerTree']['{0;0}']
 lines = [rhino3dm.CommonObject.Decode(json.loads(item['data'])) for item in branch]
 
 filename = 'twisty.3dm'
