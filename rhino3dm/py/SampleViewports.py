148706b44cdd1746da34397cd0ed54622a895e0f
fraguada <fraguada@gmail.com>
2023-10-09 12:09:00 +0200

    Adding rhino3dm python viewport sample

---
+++ rhino3dm/py/SampleViewports.py
@@ -0,0 +1,34 @@
+import rhino3dm
+model = rhino3dm.File3dm.Read('../models/Rhino_Logo_Mesh8_NamedViews.3dm')
+
+def viewportInfo(vp):
+    print("IsValidCameraFrame: " + str(vp.IsValidCameraFrame))
+    print("IsValidCamera: " + str(vp.isValidCamer))
+    print("IsValidFrustum: " + str(vp.IsValidFrustum))
+    print("IsParallelProjection: " + str(vp.IsParallelProjection))
+    print("IsPerspectiveProjection: " + str(vp.IsPerspectiveProjection))
+    print("IsTwoPointPerspectiveProjection: " + str(vp.IsTwoPointPerspectiveProjection))
+    print("CameraLocation: " + str(vp.CameraLocation))
+    print("CameraDirection: " + str(vp.CameraDirection))
+    print("CameraUp: " + str(vp.CameraUp))
+    print("CameraX: " + str(vp.CameraX))
+    print("CameraY: " + str(vp.CameraY))
+    print("CameraZ: " + str(vp.CameraZ))
+    print("GetFrustum: " + str(vp.GetFrustum))
+    print("GetScreenPort: " + str(vp.GetScreenPort))
+    print("ScreenPortAspect: " + str(vp.ScreenPortAspect))
+    print("CameraAngle: " + str(vp.CameraAngle))
+    print("Camera35mmLensLength: " + str(vp.Camera35mmLensLength))
+    print("TargetPoint: " + str(vp.TargetPoint))
+    print("Id: " + str(vp.Id))
+    print("")
+
+print("--- Views ---")  
+for view in model.Views:
+    print(view.Name)
+    viewportInfo(view.Viewport)
+
+print("--- Named Views ---") 
+for namedView in model.NamedViews:
+    print(namedView.Name)
+    viewportInfo(namedView.Viewport)
