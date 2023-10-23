8161b2cc8c2614313915af3bd689c61a77dd4b2c
fraguada <fraguada@gmail.com>
2023-10-23 22:37:26 +0200

    rhino3dm - adding material example

---
+++ rhino3dm/cs/SampleCSPBRMaterial/Program.cs
@@ -0,0 +1,24 @@
+﻿Rhino.FileIO.File3dm file3dm = new Rhino.FileIO.File3dm();
+
+var material = new Rhino.DocObjects.Material();
+material.ToPhysicallyBased();
+material.PhysicallyBased.BaseColor = new Rhino.Display.Color4f(1, 0, 0, 0);
+material.PhysicallyBased.Metallic = 0.7;
+material.PhysicallyBased.Roughness = 0.5;
+file3dm.AllMaterials.Add(material);
+
+var oa = new Rhino.DocObjects.ObjectAttributes
+{
+    MaterialIndex = 0,
+    MaterialSource = Rhino.DocObjects.ObjectMaterialSource.MaterialFromObject
+};
+
+var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 10);
+file3dm.Objects.AddSphere(sphere, oa);
+
+file3dm.Write("fileWithMaterial.3dm", null);
+
+
+
+
+
