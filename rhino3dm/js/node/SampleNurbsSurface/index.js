4801cd1d224f55de27c86cf5e3c56ed38410b9cd
fraguada <fraguada@gmail.com>
2024-03-18 15:44:54 +0100

    adding a create nurbs surface sample

--- rhino3dm/js/node/SampleNurbsSurface/index.js
+++ rhino3dm/js/node/SampleNurbsSurface/index.js
@@ -4,70 +4,41 @@ import rhino3dm from 'rhino3dm'
 const rhino = await rhino3dm()
 console.log('Loaded rhino3dm.')
 
-const buffer = fs.readFileSync('../../../models/nurbssurface.3dm')
-const arr = new Uint8Array(buffer)
-const file3dm = rhino.File3dm.fromByteArray(arr)
+const rational = false
+const dim = 3
+const u_cv_count = 6
+const v_cv_count = 6
+const u_degree = 3
+const v_degree = 3
+const u_order = u_degree + 1
+const v_order = v_degree + 1
 
-const objects = file3dm.objects()
-const cnt = objects.count
+const ns = rhino.NurbsSurface.create(dim, rational, u_order, v_order, u_cv_count, v_cv_count)
 
-console.log(`Number of Objects: ${cnt}`)
+//geberate knots
+ns.knotsU().createUniformKnots(1.0)
+ns.knotsV().createUniformKnots(1.0)
 
-
-
-for ( let i = 0; i < cnt; i ++ ) {
-    const object = objects.get( i )
-    const brep = object.geometry()
-
-    console.log(`Brep Vertices Count: ${brep.vertices().count}`)
-    console.log(`Brep Vertex: ${brep.vertices().get(3).location}`)
-    console.log(`Brep Vertex Index: ${brep.vertices().get(3).vertexIndex}`)
-    console.log(`Brep Edge Count: ${brep.vertices().get(3).edgeCount}`)
-    console.log(`Brep Edge Indices: ${brep.vertices().get(3).edgeIndices}`)
-    console.log(`Brep Edge Index: ${brep.vertices().get(3).edgeIndices[1]}`)
-
-    const surface = brep.surfaces().get( 0 )
-
-    console.log( surface.domain( 0 ) ) //domain in u
-    console.log( surface.domain( 1 ) ) //domain in v
-
-    surface.setDomain( 0, [0,1] )
-    surface.setDomain( 1, [0,1] )
-
-    console.log( surface.domain( 0 ) ) //domain in u
-    console.log( surface.domain( 1 ) ) //domain in v
-
-
-    const nurbsSurface = surface.toNurbsSurface()
-
-    const knotsU = nurbsSurface.knotsU()
-    const knotsV = nurbsSurface.knotsV()
-    console.log(knotsU.toList())
-    console.log(knotsV.toList())
-
-    /*
-    const knots = geometry.knots()
-    console.log(knots.get(5))
-    const knotList = knots.toList()
-    console.log(knotList)
-
-    */
-
-    const n_surface = rhino.NurbsSurface.create(3, nurbsSurface.isRational, nurbsSurface.orderU, nurbsSurface.orderV, nurbsSurface.points.countU, nurbsSurface.points.countV );
-    n_surface.points = nurbsSurface.points;
-    n_surface.knotsU = nurbsSurface.knotsU;
-    n_surface.knotsV = nurbsSurface.knotsV;
-    n_surface.setDomain(0, [0,1]); 
-    n_surface.setDomain(1, [0,1]);
-
-    file3dm.objects().add(n_surface, null);
-    
+//create points
+for (let u = 0; u < u_cv_count; u++) {
+    for (let v = 0; v < v_cv_count; v++) {
+        ns.points().set(u, v, [u, v, u+v, 1.0])
+    }
 }
 
+console.log('Is Surface Valid: ' + ns.isValid)
 
+const doc = new rhino.File3dm()
+
+const layer_index = doc.layers().addLayer( 'NurbsSurface', { r: 255, g: 0, b: 0, a: 255 } )
+const oa = new rhino.ObjectAttributes()
+oa.layerIndex = layer_index
+
+const uuid = doc.objects().add(ns, oa);
+console.log('id of object added to doc: ' + uuid)
 
 const options = new rhino.File3dmWriteOptions()
 options.version = 8
-let buff = file3dm.toByteArrayOptions(options)
-fs.writeFileSync("file.3dm", buff);
+let buff = doc.toByteArrayOptions(options)
 
+fs.writeFileSync("file.3dm", buff);
