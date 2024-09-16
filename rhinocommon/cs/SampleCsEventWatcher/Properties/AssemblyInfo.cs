592fd4cd15de2c85a52e410e9cf34c9e3e57330a
Dale Fugier <dale@mcneel.com>
2024-09-16 13:03:57 -0700

    Added SampleCsAddGroupUserData.cs, some missing AssemblyInfo.cs files.

---
+++ rhinocommon/cs/SampleCsEventWatcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using Rhino.PlugIns;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: PlugInDescription(DescriptionType.Address, "146 North Canal Street, Suite 320\r\nSeattle, WA 98103")]
+[assembly: PlugInDescription(DescriptionType.Country, "United States")]
+[assembly: PlugInDescription(DescriptionType.Email, "dale@mcneel.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "206-545-6877")]
+[assembly: PlugInDescription(DescriptionType.Fax, "206-545-7321")]
+[assembly: PlugInDescription(DescriptionType.Organization, "Robert McNeel & Associates")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://github.com/mcneel/rhino-developer-samples")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "https://github.com/mcneel/rhino-developer-samples")]
+[assembly: PlugInDescription(DescriptionType.Icon, "SampleCsEventWatcher.Resources.SampleCs.ico")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("934955FB-33BB-4F6C-9F9E-9B0927854AAE")]
