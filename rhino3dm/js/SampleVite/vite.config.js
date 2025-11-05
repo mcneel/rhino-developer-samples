e5e39cbedfadd42059debca7f5b341119d13c7a2
Arend van Waart <arend@studioavw.nl>
2025-11-05 10:56:51 +0100

    fix: simpler reproducable setup

--- rhino3dm/js/SampleVite/vite.config.js
+++ rhino3dm/js/SampleVite/vite.config.js
@@ -3,4 +3,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['rhino3dm'],
   },
-});
+  // use the rhino3dm.module.js entrypoint
+  resolve: {
+    alias: {
+      'rhino3dm': path.resolve(__dirname, 'node_modules/rhino3dm/rhino3dm.module.js')
+    }
+  }
+})
